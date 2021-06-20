ad45b0c7b65274ecb37b4dd358de6927fee25239
AagmanSuri <63902687+AagmanSuri@users.noreply.github.com>
2021-06-20 09:02:04 +0530

    Heading

--- src/Heading/Heading.js
+++ src/Heading/Heading.js
@@ -3,7 +3,8 @@ import './Heading.css'
 function Heading() {
     return (
         <div>
-            <h1>hello</h1>
+            <h1>Our offer Data Science and <br></br>Analytics Services</h1>
+            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</p>
         </div>
     )
 }
